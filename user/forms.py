0a16468400867b6992a6e13d15731d1c2081db81
sooshiance <s.katafract@gmail.com>
2024-09-12 11:22:06 +0330

    fix: Fix template does not exist for user app

--- user/forms.py
+++ user/forms.py
@@ -13,6 +13,21 @@ class RegisterUser(UserCreationForm):
         model = User
         fields = ['username','email','first_name','first_name']
 
+        labels = {
+            'password1': 'گذر واژه',
+            'password2': 'تکرار گذر واژه',
+        }
+
+        widgets = {
+            'email': forms.EmailInput(attrs={'class':'form-control my-5', 'placeholder':'example1234@gmail.com'}),
+            'phone': forms.NumberInput(attrs={'class':'form-control my-5', 'placeholder':'09123456789'}),
+            'username': forms.TextInput(attrs={'class':'form-control my-5', 'placeholder':'Username'}),
+            'password1': forms.PasswordInput(attrs={'class':'form-control my-5', 'placeholder':'••••••••••••'}),
+            'password2': forms.PasswordInput(attrs={'class':'form-control my-5', 'placeholder':'••••••••••••'}),
+            'first_name': forms.TextInput(attrs={'class':'form-control my-5', 'placeholder':'Ali'}),
+            'last_name': forms.TextInput(attrs={'class':'form-control my-5', 'placeholder':'alizadeh'}),
+        }
+
     def clean(self) -> dict[str, Any]:
         cleaned_data = super(RegisterUser, self).clean()
         password1 = cleaned_data.get("password1")
